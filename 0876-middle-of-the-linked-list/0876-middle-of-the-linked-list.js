9fcbd357726ae0bb59e627560b0fda05deca01ec
ekansh0202 <ekansh.anand02@gmail.com>
2026-08-03 20:19:12 +0530

    Time: 0 ms (100%), Space: 54.1 MB (18.08%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.js
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.js
@@ -13,6 +13,10 @@ var middleNode = function(head) {
     // we can use slow and fast pointer approach
     let slow = head, fast = head;
 
+    // We move 'fast' by 2 nodes (fast.next.next), so BOTH fast and fast.next
+    // must exist. Using || is unsafe because the loop may run when fast.next
+    // is null, causing fast.next.next to throw an error.
+
     while(fast !== null && fast.next !== null){
         slow = slow?.next;
         fast = fast?.next?.next;
